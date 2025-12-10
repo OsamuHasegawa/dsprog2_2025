61f84b6146af976078048f47f8df6beaf95135e6
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2025-12-10 12:28:03 +0900

    :recycle: 型が一致していない問題を修正

--- lecture-4/calculator/src/calc.py
+++ lecture-4/calculator/src/calc.py
@@ -88,7 +88,7 @@ class CalculatorApp(ft.Container):
 
     def button_clicked(self, e):
         data = e.control.data
-        print(f"Button clicked with data = {data}")
+        print(f"Button clicked with data = {data, type(data)}")
         if self.result.value == "Error" or data == "AC":
             self.result.value = "0"
             self.reset()
@@ -98,10 +98,10 @@ class CalculatorApp(ft.Container):
                 self.result.value = data
                 self.new_operand = False
             else:
-                self.result.value = self.result.value + data
+                self.result.value = str(self.result.value) + str(data)
 
         elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(self.operand1, float(self.result.value), self.operator)
+            self.result.value = str(self.calculate(self.operand1, float(str(self.result.value)), self.operator))
             self.operator = data
             if self.result.value == "Error":
                 self.operand1 = "0"
@@ -110,19 +110,19 @@ class CalculatorApp(ft.Container):
             self.new_operand = True
 
         elif data in ("="):
-            self.result.value = self.calculate(self.operand1, float(self.result.value), self.operator)
+            self.result.value = str(self.calculate(self.operand1, float(str(self.result.value)), self.operator))
             self.reset()
 
         elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
+            self.result.value = str(float(str(self.result.value)) / 100)
             self.reset()
 
         elif data in ("+/-"):
-            if float(self.result.value) > 0:
+            if float(str(self.result.value)) > 0:
                 self.result.value = "-" + str(self.result.value)
 
-            elif float(self.result.value) < 0:
-                self.result.value = str(self.format_number(abs(float(self.result.value))))
+            elif float(str(self.result.value)) < 0:
+                self.result.value = str(self.format_number(abs(float(str(self.result.value)))))
 
         self.update()
 
